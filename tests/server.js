409f96c520127672c7ba38df37637e7c7b1fafe2
Eva Ak <evaaksoy9@gmail.com>
2026-05-23 22:06:32 -0400

    Update server port Render

--- tests/server.js
+++ tests/server.js
@@ -11,8 +11,10 @@ async function startServer() {
 
         console.log("MongoDB Connected");
 
-        app.listen(process.env.PORT, () => {
-            console.log(`Server running on port ${process.env.PORT}`);
+        const PORT = process.env.PORT || 3000;
+
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
         });
 
     } catch (error) {
