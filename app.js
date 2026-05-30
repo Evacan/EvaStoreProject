13f62d41b539161372ff905e965492f6aa06c9e0
Eva Ak <evaaksoy9@gmail.com>
2026-05-30 16:25:30 -0400

    Fixed frontend homepage

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static("public"));
+
 const productRoutes = require("./routes/products");
 app.use("/", productRoutes);
 
