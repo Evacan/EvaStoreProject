679664ab476b372d12b34f76c6ecc9ad3760a887
Eva Ak <evaaksoy9@gmail.com>
2026-05-30 16:36:48 -0400

    Fix homepage route

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 const productRoutes = require("./routes/products");
+
 app.use("/", productRoutes);
 
 module.exports = app;
